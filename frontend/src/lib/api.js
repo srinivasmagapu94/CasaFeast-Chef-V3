5588b9d484b8038977eec7fa9fa031f32e02ab91
Srinivas Magapu <srinivasmagapu94>
2026-09-13 12:29:44 +0530

    Update CHEF_SERVICES_URL to use local endpoint for development

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 export const API = `${BACKEND_URL}/api`;
-const CHEF_SERVICES_URL = process.env.REACT_APP_CASAFEAST_CHEF_SERVICES_URL;
+const CHEF_SERVICES_URL = "http://localhost:8080/ws_casafeast_chef_services/chef";
 
 // Lightweight payload hash to simulate "Data in Transit Protection"
 function hashPayload(data) {
